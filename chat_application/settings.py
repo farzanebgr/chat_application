795a3fed5175e558dd9a4b7e63c5e639577110ac
farzanebgr <farzenehboogari@gmail.com>
2022-09-04 11:00:07 +0430

    index page for get chat room name

--- chat_application/settings.py
+++ chat_application/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # INTERNAL APPS
+    'chat',
+    # EXTERNAL APPS
+
 ]
 
 MIDDLEWARE = [
