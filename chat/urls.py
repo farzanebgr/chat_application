3196a8b8dd8b7a52074ff0df8d72dfe92c0937b4
farzanebgr <farzenehboogari@gmail.com>
2022-09-04 12:16:13 +0430

    Create the new file consumers.py

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from chat.views import index
+from chat.views import index, room
 
 urlpatterns = [
     path('', index, name='index-page'),
+    path('<str:room_name>/', room, name='room-page'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Get Chat Room name from user ...
 def index(request):
     return render(request, 'chat/index.html')
+
+
+# Show a Particular Chat Room name  ...
+def room(request, room_name):
+    context ={
+        'room_name': room_name
+    }
+    return render(request, 'chat/room.html',context)
