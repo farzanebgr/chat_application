509216faf0508f8b0ffcb7ac3e05d0d2d2a5229a
farzanebgr <farzenehboogari@gmail.com>
2022-09-05 10:21:33 +0430

    ChatConsumer changed...

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,9 +1,18 @@
 from channels.generic.websocket import WebsocketConsumer
+import json
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        pass
+        # Accepts an incoming socket
+        self.accept()
     def disconnect(self, close_code):
         pass
     def receive(self, text_data):
-        pass
+        # Change type from json to python object ...
+        text_data_dict = json.loads(text_data)
+        # Get value of python object from key message ...
+        message = text_data_dict['message']
+        # Reply message by type json
+        self.send(text_data=json.dumps({
+            'message':message
+        }))
